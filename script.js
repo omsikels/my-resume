c64c41b90ea36a3fef294b8b781db612af31c394
omsikels <126590017+omsikels@users.noreply.github.com>
2026-01-30 20:20:25 +0800

    Add scroll animation functionality
    Implement scroll animation for elements and skill bars.

--- script.js
+++ script.js
@@ -3,3 +3,27 @@ const toggleButton = document.getElementById("themeToggle");
 toggleButton.addEventListener("click", () => {
   document.body.classList.toggle("dark");
 });
+
+//Animate on scroll
+const animatedElements = document.querySelectorAll(".animate");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+
+        //Animate Skill Bars
+        const fills = entry.target.querySelectorAll(".fill");
+        fills.forEach((fill) => {
+          fill.style.width = fill.getAttribute("style").split(":")[1];
+        });
+
+        observer.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: 0.3 }
+);
+
+animatedElements.forEach((el) => observer.observe(el));
